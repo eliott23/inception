a722490a6d70b02b59908d98ac4aa2656593d1f9
Abderrahmane Ababachir <aababach@c4r2p7.1337.ma>
2023-09-04 17:48:43 +0100

    ana_gelt_salat_wlaho_a3lam

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -7,5 +7,5 @@ wp core download --path=/var/www/html --allow-root
 wp config create --dbhost=mariadb --dbname=wpclidemo --dbuser=aababach --dbpass=pass123 --allow-root
 
 # wp db create --allow-root
-wp core install --url=wp --title="WP-CLI" --admin_user=aababach --admin_password=pass123 --admin_email=info@wp-cli.org --allow-root
+wp core install --url=aababach.42.fr --title="WP-CLI" --admin_user=aababach --admin_password=pass123 --admin_email=info@wp-cli.org --allow-root
 php-fpm7.4 -RF
