886997849b8a94351a70bee7c5868ecaf2b0a473
Abderrahmane Ababachir <aababach@c4r2p7.1337.ma>
2023-09-04 20:59:37 +0100

    mchat_20_dh

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -3,7 +3,6 @@ service mariadb start
 mariadb -u root -e "CREATE DATABASE $DB;"
 mariadb -u root -e "CREATE USER '$USER'@'%' IDENTIFIED BY '$PASS';"
 mariadb -u root -e "GRANT ALL PRIVILEGES ON $DB.* TO '$USER';"
-mariadb -u root -e "FLUSH PRIVILEGES;"
 service mariadb stop
 
 sed -i '30c\bind-address            = 0.0.0.0' /etc/mysql/mariadb.conf.d/50-server.cnf
